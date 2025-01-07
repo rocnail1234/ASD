af196df6e44a238184036e38fa0b871596f45c00
rocnail1234 <apicondominios@gmail.com>
2025-01-06 21:39:19 -0400

    add cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,23 @@ import mainRouter from "./presentation/route"
 
    const app = express()
    const PORT = process.env.PORT || 8000
-   app.use(cors())
+   const allowedOrigins = [ 'http://localhost:5173', 'https://ninedream.netlify.app/' ]; 
+   app.use(cors({
+      origin: function (origin, callback) {
+        // Permitir solicitudes sin un origen (por ejemplo, CURL en terminal)
+        if (!origin) return callback(null, true);
+    
+        // Revisar si el origen está en la lista permitida
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+    
+        return callback(null, true);
+      }
+    }));
+    
+   
    app.use(express.json())
 
   
