2d3a9b42692023f11541852aaabb2a20901f9e63
rocnail1234 <apicondominios@gmail.com>
2025-01-15 23:00:59 -0400

    trle changte

--- src/services/payment/editPayment.ts
+++ src/services/payment/editPayment.ts
@@ -3,67 +3,66 @@ import { prisma } from "../../db/prisma"
 import { editExpense } from "../Expense"
 
 export const editPayment = async (data: EditPayment) => {
-  try {
-    const { id, community_id, expenses, ...rest } = data;
-
-    const existPayment = await prisma.payment.findUnique({
-      where: {
-        id,
-        Account: { community_id },
-      },
-    });
-
-    if (!existPayment) throw new Error('pago no encontrado');
-
-    const payment = await prisma.payment.update({
-      where: { id },
-      data: { ...rest },
-    });
-
-    if (payment.status === 'Paid' && existPayment.status === 'Pending') {
-      await prisma.account.update({
-        where: { id: payment.account_id },
-        data: {
-          balance: {
-            increment: payment.amount,
-          },
+    try {
+      const { id, community_id, expenses, ...rest } = data;
+  
+      const existPayment = await prisma.payment.findUnique({
+        where: {
+          id,
+          Account: { community_id },
         },
       });
-
-      if (expenses) {
-        await Promise.all(expenses.map(editExpense));
-      }
-    }
-
-    if (payment.status === 'Pending' && existPayment.status === 'Paid') {
-      await prisma.account.update({
-        where: { id: payment.account_id },
-        data: {
-          balance: {
-            decrement: payment.amount,
-          },
-        },
+  
+      if (!existPayment) throw new Error('pago no encontrado');
+  
+      const payment = await prisma.payment.update({
+        where: { id },
+        data: { ...rest },
       });
-
-      if (expenses) {
-        const existingExpenses = await prisma.expense.findMany({
-          where: { payment_id: payment.id },
-          select: { value: true, id: true },
+  
+      if (payment.status === 'Paid' && existPayment.status === 'Pending') {
+        await prisma.account.update({
+          where: { id: payment.account_id },
+          data: {
+            balance: {
+              increment: payment.amount,
+            },
+          },
         });
-
-        await Promise.all(
-          existingExpenses.map((expense) =>
-            prisma.expense.update({
-              where: { id: expense.id },
-              data: { owedValue: expense.value },
-            })
-          )
-        );
+  
+        if (expenses) {
+          await Promise.all(expenses.map(editExpense));
+        }
+      } else if (payment.status === 'Pending' && existPayment.status === 'Paid') {
+        await prisma.account.update({
+          where: { id: payment.account_id },
+          data: {
+            balance: {
+              decrement: payment.amount,
+            },
+          },
+        });
+  
+        if (expenses) {
+          const existingExpenses = await prisma.expense.findMany({
+            where: { payment_id: payment.id },
+            select: { value: true, id: true },
+          });
+  
+          await Promise.all(
+            existingExpenses.map((expense) =>
+              prisma.expense.update({
+                where: { id: expense.id },
+                data: { owedValue: expense.value },
+              })
+            )
+          );
+        }
       }
+  
+      return payment;
+    } catch (error) {
+      throw error;
     }
-
-    return payment;
-  } catch (error) {
-    throw error;
-  }
-};
+  };
+  
